bfeae6eabc83019f7b4e430f1fd889c90f10a50c
Simon <simonmansson@live.se>
2024-12-15 16:48:34 +0100

    now able to draw, and added a basic colorpicker 4 colors

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 console.log(ctx);
 
-canvas.style.background = "blue";
+canvas.style.background = "white";
 
 
 
@@ -15,18 +15,48 @@ resizeCanvas();
 window.addEventListener("resize", resizeCanvas);
 
 
-function drawCircle(x, y) {
-  ctx.beginPath();
-  const radius = 50;
-  ctx.arc(x, y, radius, 0, 2 * Math.PI);
-  ctx.fillStyle = "green";
-  ctx.fill();
-}
+let currentColor = "black";
+
+document.querySelectorAll(".color-btn").forEach(button => {
+  button.addEventListener("click", function() {
+    currentColor = this.dataset.color;
+  });
+});
+
+
+
+
+
+
+
+
+
+
 
 canvas.addEventListener("touchstart", function(ev) {
+  const touch = ev.touches[0]; 
+  const x = touch.clientX;
+  const y = touch.clientY;
+
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  
+});
+
+
+
+canvas.addEventListener("touchmove", function(ev) {
   const touch = ev.touches[0];
   const x = touch.clientX;
   const y = touch.clientY;
 
-  drawCircle(x, y);
+  ctx.lineTo(x, y);
+  ctx.stroke();
+  ctx.strokeStyle = currentColor;
+  ctx.lineWidth = 5;
 });
+
+
+
+
+
